208e38ce81d0dcbf957fb0e5bf6cc71f4c76fc14
Fleup <liebenstein.stefan@gmail.com>
2016-11-30 16:31:31 +0100

    more tests,

--- tests/test_polygon_utilities.py
+++ tests/test_polygon_utilities.py
@@ -9,14 +9,21 @@ from python_geometry.polygon_utilities import area_of_polygon_2D
 
 
 class TestArea_of_polygon2D(TestCase):
-    def test_quads(self):
+    def test_rectangles(self):
+        ## square
         quad1 = ([0, 0], [1, 0], [1, 1], [0, 1])
         quad2 = ([0, 0], [0, 1], [1, 1], [1, 0])
         assert_equal(area_of_polygon_2D(quad1), 1)
         assert_equal(area_of_polygon_2D(quad2), -1)
 
+        # rectangles
+        quad3 = ([0, 0], [5, 0], [5, 1], [0, 1])
+        assert_equal(area_of_polygon_2D(quad3), 5)
+
     def test_triangles(self):
         tria1 = ([0, 0], [1, 0], [1, 1])
         tria2 = ([0, 0], [1, 1], [1, 0])
+        tria3 = ([0, 0], [5, 10], [2, 5])
         assert_equal(area_of_polygon_2D(tria1), 0.5)
         assert_equal(area_of_polygon_2D(tria2), -0.5)
+        assert_equal(area_of_polygon_2D(tria3), 2.5)
